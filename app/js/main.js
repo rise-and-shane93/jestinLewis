e9650e1f3bf03ad7d46df57482032901c28b2202
rise_and_shane93 <cdtharper@gmail.com>
2019-03-06 21:57:20 -0500

    add about section

--- app/js/main.js
+++ app/js/main.js
@@ -38,5 +38,59 @@ if ( navigator.userAgent.match(/Android/i)
         navbar.classList.add("bg-light", "navbar-light");
         //navbar.classList.add("navbar-light");
     } else {
-        navbar.classList.remove("bg-light");
+        //navbar.classList.remove("bg-light");
+        navbar.classList.add("navbar-dark");
     }
+
+//sets random image on about section
+const aboutImg = document.querySelector('.tuRandomImg');
+
+//Adena random image selector
+
+//for large screens (Portrait)
+var tuImagesPort = [
+    "images/jestin/portrait/jLewPort1.jpg",
+    "images/jestin/portrait/jLewPort2.jpg",
+    "images/jestin/portrait/jLewPort3.jpg",
+    "images/jestin/portrait/jLewPort4.jpg",
+    "images/jestin/portrait/jLewPort5.jpg",
+    "images/jestin/portrait/jLewPort6.jpg",
+    "images/jestin/portrait/jLewPort7.jpg",
+    "images/jestin/portrait/jLewPort8.jpg",
+    "images/jestin/portrait/jLewPort9.jpg"
+];
+
+//for small screens (Landscape)
+var tuImagesLand = [
+    "images/jestin/landscape/jLewLand1.jpg",
+    "images/jestin/landscape/jLewLand2.jpg",
+    "images/jestin/landscape/jLewLand3.jpg",
+    "images/jestin/landscape/jLewLand4.jpg",
+    "images/jestin/landscape/jLewLand5.jpg",
+    "images/jestin/landscape/jLewLand6.jpg",
+    "images/jestin/landscape/jLewLand7.jpg"
+];
+
+function getRandomImageAbout() {
+    var img = '<img alt=\"An image of Jestin Lewis.\" class=\"img-fluid\" src=\"';
+    var randomImg;
+    if ( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+        ) {
+        // dailyQuote.innerHTML = getRandomQuote();
+        randomImg = Math.floor(Math.random() * tuImagesLand.length);
+        img += tuImagesLand[randomImg];
+    } else { 
+        randomImg = Math.floor(Math.random() * tuImagesPort.length);
+        img += tuImagesPort[randomImg];
+    }
+    img += '\">';
+    return img;
+}; 
+
+aboutImg.innerHTML = getRandomImageAbout();
