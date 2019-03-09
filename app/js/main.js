02de508d6de6003cc85976a1354810b05b8a139f
rise_and_shane93 <cdtharper@gmail.com>
2019-03-09 11:15:53 -0500

    modify shop section

--- app/js/main.js
+++ app/js/main.js
@@ -8,16 +8,10 @@ $play.hide();
 $pausePlay.click(function() {
     if($(this).hasClass('fa-pause')) {
         $lewisVideo.trigger('pause');
-        //$play.css('z-index', '12'); 
-        // $pause.css('display', 'none');
-        // $play.css('display', 'block'); 
         $pause.hide();
         $play.show();
     } else {
         $lewisVideo.trigger('play');
-        //$play.css('z-index', '10');  
-        // $play.css('display', 'none');
-        // $pause.css('display', 'block');  
         $play.hide();
         $pause.show();
     }
